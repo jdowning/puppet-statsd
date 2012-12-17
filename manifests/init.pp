8de411e05bb37e8f0a9f48e12fbd25c770f0e411
Etienne Pelletier <epelletier@maestrodev.com>
2012-12-17 11:55:29 -0600

    Added support for additional backends and RHEL init script.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,15 @@
 class statsd(
-  $graphiteserver,
+  $graphiteserver   = 'localhost',
   $graphiteport     = '2003',
+  $backends         = [ 'graphite' ],
   $address          = '0.0.0.0',
   $listenport       = '8125',
   $flushinterval    = '10000',
   $percentthreshold = ['90'],
   $ensure           = 'present',
   $provider         = 'npm',
+  $config           = { },
+  $node_module_dir  = '',
 ) {
 
   require nodejs
@@ -15,9 +18,38 @@ class statsd(
     ensure   => $ensure,
     provider => $provider,
   }
+  
+  if ! $node_module_dir {
+    case $provider {
+      'apt': {
+        $statsjs = '/usr/share/statsd/stats.js'
+      }
+      'npm': {
+        $statsjs = '/usr/lib/node_modules/statsd/stats.js'
+      }
+      default: {
+        fail('Unsupported provider')
+      }
+    }
+  }
+  else {
+    $statsjs = "${node_module_dir}/statsd/stats.js"
+  }
 
   $configfile = '/etc/statsd/localConfig.js'
   $logfile    = '/var/log/statsd/statsd.log'
+  
+  case $::osfamily {
+    'RedHat': {
+      $init_script = 'puppet:///modules/statsd/statsd-init-rhel'
+    }
+    'Debian': {
+      $init_script = 'puppet:///modules/statsd/statsd-init'
+    }
+    default: {
+      fail('Unsupported OS Family')
+    }
+  }
 
   file {
     '/etc/statsd':
@@ -32,7 +64,7 @@ class statsd(
       mode    => '0444',
       require => Package['statsd'];
     '/etc/init.d/statsd':
-      source  => 'puppet:///modules/statsd/statsd-init',
+      source  => $init_script,
       owner   => 'root',
       group   => 'root',
       mode    => '0755';
@@ -44,7 +76,7 @@ class statsd(
     '/var/log/statsd':
       ensure => directory,
       owner  => 'root',
-      group  => 'nogroup',
+      group  => 'root',
       mode   => '0770';
     '/usr/local/sbin/statsd':
       source  => 'puppet:///modules/statsd/statsd-wrapper',
@@ -56,7 +88,7 @@ class statsd(
   service { 'statsd':
     ensure    => running,
     enable    => true,
-    hasstatus => false,
+    hasstatus => true,
     pattern   => 'node .*stats.js',
     require   => File['/var/log/statsd'],
     subscribe => [
