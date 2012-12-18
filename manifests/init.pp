0e2bb8b35bf3448a704d7501b4e7207244eed2ef
Etienne Pelletier <epelletier@maestrodev.com>
2012-12-18 14:57:08 -0600

    Cleaned up a few things. Corrected idempotency issues w config and service dependencies.

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,81 +8,61 @@ class statsd(
   $percentthreshold = ['90'],
   $ensure           = 'present',
   $provider         = 'npm',
-  $config           = { },
-  $node_module_dir  = '',
-) {
+  $config           = { }) 
+inherits statsd::params {
 
   require nodejs
 
   package { 'statsd':
     ensure   => $ensure,
     provider => $provider,
-  }
-  
-  if ! $node_module_dir {
-    case $provider {
-      'apt': {
-        $statsjs = '/usr/share/statsd/stats.js'
-      }
-      'npm': {
-        $statsjs = '/usr/lib/node_modules/statsd/stats.js'
-      }
-      default: {
-        fail('Unsupported provider')
-      }
-    }
-  }
-  else {
-    $statsjs = "${node_module_dir}/statsd/stats.js"
+    notify  => Service['statsd'],
   }
 
-  $configfile = '/etc/statsd/localConfig.js'
-  $logfile    = '/var/log/statsd/statsd.log'
-  
-  case $::osfamily {
-    'RedHat': {
-      $init_script = 'puppet:///modules/statsd/statsd-init-rhel'
-    }
-    'Debian': {
-      $init_script = 'puppet:///modules/statsd/statsd-init'
-    }
-    default: {
-      fail('Unsupported OS Family')
-    }
-  }
+  $configfile  = '/etc/statsd/localConfig.js'
+  $logfile     = '/var/log/statsd/statsd.log'
+  $statsjs     = $statsd::params::statsjs
+  $init_script = $statsd::params::init_script
 
-  file {
-    '/etc/statsd':
-      ensure => directory,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755';
-    $configfile:
-      content => template('statsd/localConfig.js.erb'),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0444',
-      require => Package['statsd'];
-    '/etc/init.d/statsd':
-      source  => $init_script,
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755';
-    '/etc/default/statsd':
-      content => template('statsd/statsd-defaults.erb'),
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755';
-    '/var/log/statsd':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0770';
-    '/usr/local/sbin/statsd':
-      source  => 'puppet:///modules/statsd/statsd-wrapper',
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0755';
+  file { '/etc/statsd':
+    ensure => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+  } ->
+  file { $configfile:
+    content => template('statsd/localConfig.js.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    notify  => Service['statsd'],
+  }
+  file { '/etc/init.d/statsd':
+    source  => $init_script,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    notify  => Service['statsd'],
+  }
+  file {  '/etc/default/statsd':
+    content => template('statsd/statsd-defaults.erb'),
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    notify  => Service['statsd'],
+  }
+  file { '/var/log/statsd':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0770',
+  }
+  file { '/usr/local/sbin/statsd':
+    source  => 'puppet:///modules/statsd/statsd-wrapper',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    notify  => Service['statsd'],
   }
 
   service { 'statsd':
@@ -91,12 +71,5 @@ class statsd(
     hasstatus => true,
     pattern   => 'node .*stats.js',
     require   => File['/var/log/statsd'],
-    subscribe => [
-      File['/etc/init.d/statsd'],
-      File['/etc/default/statsd'],
-      File['/usr/local/sbin/statsd'],
-      File[$configfile],
-      Package['statsd'],
-    ],
   }
 }
