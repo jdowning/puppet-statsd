9dd7a9f65751f9dc214514add4ea9eb3f5cf7112
Justin Downing <justin@downing.us>
2014-04-13 15:07:58 -0400

    Release 1.1.0

--- tests/vagrant.pp
+++ tests/vagrant.pp
@@ -1,5 +1,9 @@
 class { 'nodejs': manage_repo => true }->
+# http://blog.npmjs.org/post/78085451721/npms-self-signed-certificate-is-no-more
+exec { '/usr/bin/npm config set ca=""': }->
 class { 'statsd':
-  backends      => [ './backends/graphite', 'statsd-influxdb-backend' ],
+  backends      => [ './backends/graphite', 'statsd-influxdb-backend', 'statsd-librato-backend' ],
   influxdb_host => 'localhost',
+  librato_email => 'foo@bar.com',
+  librato_token => 'secret_token',
 }
