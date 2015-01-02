405f1221328fdd71ee5b0d8702d2e1a3cb8d32d4
Justin Downing <justin@downing.us>
2015-01-02 12:27:45 -0500

    DRY backends.pp to avoide lint warning

--- manifests/backends.pp
+++ manifests/backends.pp
@@ -1,13 +1,14 @@
 # == Class: statsd::backends
 class statsd::backends {
   $backends = join($statsd::backends, ',')
+  $node_base = "${statsd::node_module_dir}/statsd/node_modules"
 
   # Make sure $statsd::influxdb_host is set
   if $backends =~ /influxdb/ {
     exec { 'install-statsd-influxdb-backend':
       command => '/usr/bin/npm install --save statsd-influxdb-backend',
       cwd     => "${statsd::node_module_dir}/statsd",
-      unless  => "/usr/bin/test -d ${statsd::node_module_dir}/statsd/node_modules/statsd-influxdb-backend",
+      unless  => "/usr/bin/test -d ${node_base}/statsd-influxdb-backend",
       require => Package['statsd'],
     }
   }
@@ -17,7 +18,7 @@ class statsd::backends {
     exec { 'install-statsd-librato-backend':
       command => '/usr/bin/npm install --save statsd-librato-backend',
       cwd     => "${statsd::node_module_dir}/statsd",
-      unless  => "/usr/bin/test -d ${statsd::node_module_dir}/statsd/node_modules/statsd-librato-backend",
+      unless  => "/usr/bin/test -d ${node_base}/statsd-librato-backend",
       require => Package['statsd'],
     }
   }
@@ -27,7 +28,7 @@ class statsd::backends {
     exec { 'install-statsd-stackdriver-backend':
       command => '/usr/bin/npm install --save stackdriver-statsd-backend',
       cwd     => "${statsd::node_module_dir}/statsd",
-      unless  => "/usr/bin/test -d ${statsd::node_module_dir}/statsd/node_modules/stackdriver-statsd-backend",
+      unless  => "/usr/bin/test -d ${node_base}/stackdriver-statsd-backend",
       require => Package['statsd'],
     }
   }
